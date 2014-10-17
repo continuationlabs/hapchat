ef8489b862b1e219e8bdb49674e89e8adb5a2091
James Weston <jwesto1@L-SB8D2YTFD5-M.local>
2014-10-17 11:44:18 -0400

    Refactor strategies

--- hapchat/lib/index.js
+++ hapchat/lib/index.js
@@ -111,3 +111,34 @@ module.exports.initPaths = function initPaths(root) {
         }
     }
 };
+
+
+module.exports.registerStrategies = function (server) {
+
+    // Set session
+    server.auth.strategy('session', 'cookie', {
+        password: 'cookie_encryption_password',
+        cookie: 'sid',
+        isSecure: false,
+        redirectTo: '/login',
+        ttl: server.app.oneDay
+    });
+
+    // Facebook third party auth
+    server.auth.strategy('facebook', 'bell', {
+        provider: 'facebook',
+        password: 'cookie_encryption_password',
+        clientId: '966006790093008',
+        clientSecret: '8473cd8260b4b60671236784b5c729b8',
+        isSecure: false
+    });
+
+    // Github third party auth
+    server.auth.strategy('github', 'bell', {
+        provider: 'github',
+        password: 'cookie_encryption_password',
+        clientId: 'd40df1c0836ce9f1ca10',
+        clientSecret: '1916660d600d84974d27911b1fe7983c7946cd8b',
+        isSecure: false
+    });
+}
